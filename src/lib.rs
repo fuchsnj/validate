a7424e0da7c7f4e40c86d917e2952940c80d111d
Nathan Fuchs <fuchsnj@gmail.com>
2017-10-11 20:12:18 -0400

    add Validate trait

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,11 @@ mod bound;
 pub mod rules;
 
 pub use rule::{Rule, ValidationResult, Error};
+
+pub trait Validate: 'static {
+	fn get_validation_rule() -> Rule<Self>;
+
+	fn validate(&self) -> ValidationResult {
+		Self::get_validation_rule().validate(self)
+	}
+}
